4c161ae792a2d5f24f7fe0e1c441b3a13c6831c9
Franklin Percicotte <43394686+frankpercicotte@users.noreply.github.com>
2022-05-07 23:29:56 -0300

    iniciando projeto com springboot

---
+++ aws-consumer/src/main/java/com/group6/awsconsumer/AwsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.group6.awsconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsConsumerApplication.class, args);
+	}
+
+}
---
+++ aws-consumer/src/test/java/com/group6/awsconsumer/AwsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.awsconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
